fc0c0bbd2c5d68997731cd86f86326fdfc219ea2
maratasdan <maratas.094747@gensan.sti.edu.ph>
2026-04-01 10:00:03 +0800

    Initial Commit

---
+++ SMAPS/SMAPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMAPSApp.swift
+//  SMAPS
+//
+//  Created by Dan on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SMAPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
