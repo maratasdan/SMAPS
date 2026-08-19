7756abd9ebbe89c55573c3a10c7eb01eec42f131
Dan <danyoumaratas@gmail.com>
2026-08-19 21:45:29 +0800

    Wed Aug 19, 9:45 PM

---
+++ SMAPS/Grower/GrowersInfo.swift
@@ -0,0 +1,18 @@
+//
+//  GrowersInfo.swift
+//  SMAPS
+//
+//  Created by danxd on 8/19/26.
+//
+
+import SwiftUI
+
+struct GrowersInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GrowersInfo()
+}
